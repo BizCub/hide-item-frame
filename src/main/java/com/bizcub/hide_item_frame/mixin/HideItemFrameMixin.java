1c16cbf10374f1beda026b79711e9e20fcbbbabf
PovarRavop <povar228my@gmail.com>
2025-02-01 07:22:19 +0200

    initial

---
+++ src/main/java/com/bizcub/hide_item_frame/mixin/HideItemFrameMixin.java
@@ -0,0 +1,19 @@
+package com.bizcub.hide_item_frame.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.client.render.entity.state.ItemFrameEntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemFrameEntityRenderer.class)
+public abstract class HideItemFrameMixin {
+
+    @Inject(method = "render*", at = @At(value = "HEAD"))
+    public void method1(ItemFrameEntityRenderState itemFrameEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        itemFrameEntityRenderState.invisible = !itemFrameEntityRenderState.itemRenderState.isEmpty();
+    }
+}
