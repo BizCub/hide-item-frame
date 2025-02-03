73c9c965b0ba0be6b253f13fa5641edcaad51eb7
PovarRavop <povar228my@gmail.com>
2025-02-03 06:30:44 +0200

    fix

--- src/main/java/com/bizcub/hide_item_frame/mixin/HideItemFrameMixin.java
+++ src/main/java/com/bizcub/hide_item_frame/mixin/HideItemFrameMixin.java
@@ -1,19 +1,34 @@
 package com.bizcub.hide_item_frame.mixin;
 
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
-import net.minecraft.client.render.entity.state.ItemFrameEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemFrameEntityRenderer.class)
-public abstract class HideItemFrameMixin {
+public abstract class HideItemFrameMixin<T extends ItemFrameEntity> extends EntityRenderer<T> {
 
-    @Inject(method = "render*", at = @At(value = "HEAD"))
-    public void method1(ItemFrameEntityRenderState itemFrameEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        itemFrameEntityRenderState.invisible = !itemFrameEntityRenderState.itemRenderState.isEmpty();
+    protected HideItemFrameMixin(EntityRendererFactory.Context ctx) {
+        super(ctx);
+    }
+
+    @Inject(
+            method = "render*",
+            at = @At(
+                    value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;isInvisible()Z",
+                    shift = At.Shift.AFTER),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;getYaw()F"),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockRenderManager;getModels()Lnet/minecraft/client/render/block/BlockModels;")
+            ))
+    public boolean method3(T itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        ItemStack itemStack = itemFrameEntity.getHeldItemStack();
+        return !itemStack.isEmpty();
     }
 }
