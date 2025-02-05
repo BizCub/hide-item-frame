52326700a98bcb3976fd3a0c730e4cff7f53c7b5
PovarRavop <bizcubmine@gmail.com>
2025-02-05 06:28:31 +0200

    rename method

--- src/main/java/com/bizcub/hide_item_frame/mixin/HideItemFrameMixin.java
+++ src/main/java/com/bizcub/hide_item_frame/mixin/HideItemFrameMixin.java
@@ -27,7 +27,7 @@ public abstract class HideItemFrameMixin<T extends ItemFrameEntity> extends Enti
                     from = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/Vector3f;getDegreesQuaternion(F)Lnet/minecraft/util/math/Quaternion;"),
                     to = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockRenderManager;getModels()Lnet/minecraft/client/render/block/BlockModels;")
             ))
-    public boolean method3(T itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+    public boolean hideItemFrame(T itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         ItemStack itemStack = itemFrameEntity.getHeldItemStack();
         return !itemStack.isEmpty();
     }
