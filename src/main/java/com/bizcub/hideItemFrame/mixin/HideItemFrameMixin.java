baa07420c2f9c2f47466376c59db46964dc176da
BizCub <bizcubmine@gmail.com>
2025-07-01 12:37:44 +0200

    rename folders

--- src/main/java/com/bizcub/hideItemFrame/mixin/HideItemFrameMixin.java
+++ src/main/java/com/bizcub/hideItemFrame/mixin/HideItemFrameMixin.java
@@ -1,4 +1,4 @@
-package com.bizcub.hide_item_frame.mixin;
+package com.bizcub.hideItemFrame.mixin;
 
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
