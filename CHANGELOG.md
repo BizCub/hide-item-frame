ad43b3953a1c2f8ed444b74b0ecec01539ea1003
BizCub <bizcubmine@gmail.com>
2025-07-18 10:21:12 +0200

    * Added publishing to websites * Now item frames become invisible if NBT is set to {Invisible:1b} * Added support for Forge and Neoforge

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+* Added publishing to websites
+* Now item frames become invisible if NBT is set to {Invisible:1b}
+* Added support for Forge and Neoforge
--- src/main/java/com/bizcub/hideItemFrame/mixin/HideItemFrameMixin.java
+++ src/main/java/com/bizcub/hideItemFrame/mixin/HideItemFrameMixin.java
@@ -1,5 +1,55 @@
 package com.bizcub.hideItemFrame.mixin;
 
+// 1.17 - 1.21.1
+//? if <=1.21.1 {
+
+/*import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemFrameEntityRenderer.class)
+public abstract class HideItemFrameMixin<T extends ItemFrameEntity> {
+
+    @Inject(method = "render*",  at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;isInvisible()Z", shift = At.Shift.AFTER))
+    private boolean hideItemFrame(T itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo callbackInfo) {
+        boolean invisible = itemFrameEntity.isInvisible();
+        if (!invisible) {
+            return !itemFrameEntity.getHeldItemStack().isEmpty();
+        }
+        return invisible;
+    }
+}
+
+// 1.21.2 - 1.21.3
+*///?} elif <=1.21.3 {
+
+/*import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.client.render.entity.state.ItemFrameEntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemFrameEntityRenderer.class)
+public abstract class HideItemFrameMixin {
+
+    @Inject(method = "render*", at = @At(value = "HEAD"))
+    public void hideItemFrame(ItemFrameEntityRenderState itemFrameEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        if (!itemFrameEntityRenderState.invisible) {
+            itemFrameEntityRenderState.invisible = !itemFrameEntityRenderState.contents.isEmpty();
+        }
+    }
+}
+
+// 1.21.4 -
+*///?} elif <=1.21.7 {
+
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
 import net.minecraft.client.render.entity.state.ItemFrameEntityRenderState;
@@ -14,6 +64,10 @@ public abstract class HideItemFrameMixin {
 
     @Inject(method = "render*", at = @At(value = "HEAD"))
     public void hideItemFrame(ItemFrameEntityRenderState itemFrameEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        itemFrameEntityRenderState.invisible = !itemFrameEntityRenderState.itemRenderState.isEmpty();
+        if (!itemFrameEntityRenderState.invisible) {
+            itemFrameEntityRenderState.invisible = !itemFrameEntityRenderState.itemRenderState.isEmpty();
+        }
     }
 }
+
+//?}
