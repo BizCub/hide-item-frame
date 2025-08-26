dd023f71981bc4b86e1aab0e826fc4a39623e01f
BizCub <bizcubmine@gmail.com>
2025-08-26 10:02:15 +0200

    Publish fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,8 +70,8 @@ if (localPropertiesFile.exists()) {
 }
 
 publishMods {
-    file = rootProject.file("build/libs/${mod.version}/$loader/${mod.id}-$loader-${mod.version}+${mod.mc_start}.jar")
-    displayName = "${mod.name} ${loader.replaceFirstChar { it.uppercase() }} ${mod.mc_start} v${mod.version}"
+    file = rootProject.file("build/libs/${mod.version}/$loader/$mixinId-$loader-${mod.version}+${mod.mc_start}.jar")
+    displayName = "${mod.name} ${loader.capitalize()} ${mod.mc_start} v${mod.version}"
     changelog = rootProject.file("CHANGELOG.md").readText()
     version = mod.version
     type = STABLE
